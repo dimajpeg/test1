232e9b3c0a20ddca42100ff8b0b1413cdbb6fe4f
dima <>
2025-02-15 20:19:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "24px",
+        fontWeight: "bold"
+      }}>
+        Здесь скоро будет сайт для помощи в изучении английского!
+      </div>
+  );
+}
+
+export default App;
